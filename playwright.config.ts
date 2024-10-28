34532d44d6fbb19df53b781686c606cd59e603ea
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2024-10-28 10:07:02 +0200

    Add workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['line'], ["allure-playwright"]],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: process.env.CI ? [['allure-playwright']] : [['line']],
   timeout: 30000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
